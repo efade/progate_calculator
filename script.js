9f2129155c1c7926434aa1e2b4391819eb3701de
Fakhruddin Ahmad Darwis <fakhri.satu@gmail.com>
2020-11-02 20:47:33 +0700

    Added operators listener.

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
+const calculatorScreen = document.querySelector('.calculator-screen');
+
+const updateScreen = (number) => {
+  calculatorScreen.value = number
+};
+
 const numbers = document.querySelectorAll(".number");
-console.log(numbers);
+const operators = document.querySelectorAll(".operator");
+const equalSign = document.querySelector(".equal-sign");
+
+let prevNumber = '';
+let calculationOperator = '';
+let currentNumber = '0';
+
+const inputNumber = (number) => {
+  if (currentNumber === '0') {
+    currentNumber = number;
+  } else {
+    currentNumber += number;
+  };
+};
+
+const inputOperator = (operator) => {
+  prevNumber = currentNumber;
+  calculationOperator = operator;
+  currentNumber = '';
+}
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    inputNumber(event.target.value);
+    updateScreen(currentNumber);
+  });
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    inputOperator(event.target.value);
+  });
+});
+
+equalSign.addEventListener('click', () => {
+  console.log('equal button is pressed');
+});**
